8ab06b6becc896590fd245a8d642f73f0bd8e1cf
aman213s4 <113830087+aman213s4@users.noreply.github.com>
2024-07-23 10:45:00 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,7 @@
+api_id = 25218674
+api_hash = "87c231f0e8704795b8239d06965b4351"
+bot_token = "7198600769:AAEA_EbrLFOYhMBm7pRi7BWKQgYHFdiIeTg"
+auth_users = [999741495]
+sudo_user = 1713924419
+log_channel = -1002236425890
+txt_channel = -1002236425890
